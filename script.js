71751a29485df1289d28b819b3b121c51d006247
husein zidan <97558377+ZIDANIDROS@users.noreply.github.com>
2023-12-09 22:16:23 +0700

    Update script.js
    navbar

---
+++ script.js
@@ -0,0 +1,12 @@
+let prevScrollpos = window.pageYOffset;
+const navbar = document.querySelector('.navbar');
+
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = '0';
+  } else {
+    navbar.style.top = `-${navbar.offsetHeight}px`;
+  }
+  prevScrollpos = currentScrollPos;
+};
